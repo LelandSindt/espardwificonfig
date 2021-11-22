dabf14a26d1386c24a24e5799ef7df0f98556626
Leland Sindt <leland@sway.org>
2021-11-22 17:56:53 -0600

    remove ifdiff/debug

--- espardwificonfig.ino
+++ espardwificonfig.ino
@@ -5,7 +5,6 @@
 #include <EEPROM.h>
 #include <ESPmDNS.h>
 
-#define SERIALDEBUG
 
 char ssid[33] = "";
 char password[65] = "";
@@ -28,26 +27,20 @@ IPAddress mDNSIP;
 WebServer server( 80 );
 
 void setup ( void ) {
-#ifdef SERIALDEBUG
   Serial.begin ( 115200 );
-#endif
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   eeprom_read_string(0, ssid, sizeof(ssid));
   eeprom_read_string(128, password, sizeof(password));
   eeprom_read_string(99, deviceName, sizeof(deviceName));
-#ifdef SERIALDEBUG
   Serial.println(" ");
   Serial.println(ssid);
-  Serial.println(password);
-#endif
+  //Serial.println(password);
   WiFi.mode(WIFI_STA);
   WiFi.begin ( ssid, password );
-#ifdef SERIALDEBUG
   Serial.println(" ");
   Serial.print("Connecting to: ");
   Serial.print(ssid);
   Serial.print(" ");
-#endif
   for (int i = 0; i <= 20; i++) {
     if (WiFi.status() == WL_CONNECTED ) {
       Serial.println("connected");
@@ -55,18 +48,14 @@ void setup ( void ) {
       break;
     }
     //Serial.println(WiFi.status());
-#ifdef SERIALDEBUG
     Serial.print(".");
-#endif
     delay(500);
   }
 
   if (WiFi.status() != WL_CONNECTED ) {
-#ifdef SERIALDEBUG
     Serial.println("connection failed.");
     Serial.print("Starting AP: ");
     Serial.println(apssid);
-#endif
     WiFi.mode(WIFI_AP);
     WiFi.softAP(apssid, appassword);
     mDNSIP = WiFi.softAPIP();
@@ -83,24 +72,16 @@ void setup ( void ) {
   server.on ("/led_off", led_off);
   server.onNotFound ( handleNotFound );
   server.begin();
-#ifdef SERIALDEBUG
   Serial.println ( "HTTP server started" );
-#endif
   if (!MDNS.begin(deviceName)) {
-#ifdef SERIALDEBUG
     Serial.println("Error setting up MDNS responder!");
-#endif
   } else {
-#ifdef SERIALDEBUG
     Serial.print(deviceName);
     Serial.println(".local MDNS responder started");
-#endif
     MDNS.addService("http", "tcp", 80);
   }
-#ifdef SERIALDEBUG
   Serial.print("IP address: ");
   Serial.println(mDNSIP);
-#endif
   digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED off
 } //setup()
 
